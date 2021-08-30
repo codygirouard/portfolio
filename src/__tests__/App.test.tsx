c65e2876e37836f86e7030d23c0d1e1f43afc969
Cody Girouard <codyagirouard@gmail.com>
2021-08-30 17:49:45 -0500

    move tests to __test__ folder

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 import { shallow } from 'enzyme';
 
-import App from './App';
+import App from '../App';
 
 it('renders without crashing', () => {
   shallow(<App />);
