c56a357d3d310bdf794b71ec95ad959996e2c731
Cody Girouard <codyagirouard@gmail.com>
2021-08-30 04:24:48 -0500

    Not found page styling and netlify redirect file

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
-  return <h2>Page not found</h2>;
+  return (
+    <div className="not-found">
+      <div>
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+        <Link to="/" className="button">
+          Go Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
