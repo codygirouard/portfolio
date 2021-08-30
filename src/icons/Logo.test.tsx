4a906b38c781c95540473e3805ff58c78f1e475e
Cody Girouard <codyagirouard@gmail.com>
2021-08-30 17:34:51 -0500

    verify logo tests pass

---
+++ src/icons/Logo.test.tsx
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom';
+import { mount, shallow } from 'enzyme';
+
+import Logo from './Logo';
+
+it('renders without crashing', () => {
+  shallow(<Logo letter="C" />);
+});
+
+it('allows us to set props', () => {
+  const wrapper = mount(<Logo letter="A" />);
+
+  expect(wrapper.props().letter).toEqual('A');
+  wrapper.setProps({ letter: 'B' });
+  expect(wrapper.props().letter).toEqual('B');
+});
