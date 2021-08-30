3489fb30fdeb0c7a5ef6aa84426d49d531dca161
Cody Girouard <codyagirouard@gmail.com>
2021-08-30 04:25:09 -0500

    verify mobile nav renders correctly

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
 import '@testing-library/jest-dom';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
 });
---
+++ src/header/MobileNav.test.tsx
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom';
+import { render, shallow } from 'enzyme';
+
+import MobileNav from './MobileNav';
+
+it('renders without crashing', () => {
+  shallow(<MobileNav />);
+});
+
+it('renders hamburger button', () => {
+  const wrapper = render(<MobileNav />);
+
+  expect(wrapper.find('.hamburger')).toHaveLength(1);
+  expect(wrapper.html()).toContain('input');
+});
+
+it('renders links', () => {
+  const wrapper = render(<MobileNav />);
+
+  expect(wrapper.text()).toContain('About');
+  expect(wrapper.text()).toContain('Projects');
+  expect(wrapper.text()).toContain('Contact');
+  expect(wrapper.text()).toContain('Resume');
+});
